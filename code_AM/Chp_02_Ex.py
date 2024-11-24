7dd956636bce4c042156c46b7ed07deb1fefaa7e
Andrew Mckay <amcka48@gmail.com>
2024-11-24 21:04:42 +1000

    Chapter 2 Exercises Completed

---
+++ code_AM/Chp_02_Ex.py
@@ -0,0 +1,311 @@
+#%%
+import arviz as az
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import pymc as pm
+import preliz as pz
+import pytensor
+from scipy.special import erf
+# pytensor.config.gcc__cxxflags = '-L/opt/miniconda3/envs/bap3/lib -O3 -march=native'
+# pytensor.config.cxx = '/usr/bin/clang++'
+# pytensor.config.blas__ldflags = '-framework Accelerate'
+def create_summary_table(summaries, model_labels=None):
+    import pandas as pd
+
+    if model_labels is None:
+        model_labels = [f'Model_{i+1}' for i in range(len(summaries))]
+
+    dfs = [df.T.rename(columns=lambda x: f'{x}_{label}') 
+           for df, label in zip(summaries, model_labels)]
+
+    final_df = pd.concat(dfs, axis=1)
+
+    # Optional: Reorder the index
+    index_order = ['mean', 'sd', 'hdi_3%', 'hdi_97%']
+    final_df = final_df.reindex(index_order)
+
+    # Optional: Round numerical values
+    final_df = final_df.round(3)
+
+    return final_df
+
+
+
+#%% Exercise 1
+# Change the parameters of the prior Beta distribution in our_first_model
+# to match those of the previous chapter. Compare the results to the previous chapter
+np.random.seed(123)
+trials = 4
+theta_real = 0.35  # True (but unknown) probability of success
+data = pz.Binomial(n=1, p=theta_real).rvs(trials)  # Simulating observed data
+print("Simulated data:", data)
+
+beta_params = [(1,1), (20, 20), (1, 4)] #Alpha first and beta second
+idatas = []
+if __name__ == '__main__':
+    for alpha, beta in beta_params: 
+        with pm.Model() as our_first_model:
+            theta = pm.Beta('theta', alpha=alpha, beta=beta)  # Prior for θ
+            y = pm.Bernoulli('y', p=theta, observed=data)  # Likelihood for observed data
+            idata = pm.sample(1000, random_seed=4591, cores=4, chains=4)  # Posterior sampling
+            idatas.append(idata)
+    
+    #Plot posterior for each model
+    _, axes = plt.subplots(1,3, figsize=(12, 4))
+    for idata, ax in zip(idatas, axes):
+        az.plot_posterior(idata, ax=ax)
+    plt.show()
+    #Compare uncertainty around estimates
+    az.plot_forest(idatas, combined=True, figsize=(10, 2))
+    plt.show()
+
+#%% Exercise 2
+# Change the prior for theta in our first model to uniform(0,1)
+# Are the posteriors different? Only the [-1,2] posterior is different but still centered appropriately
+# Is the sampling slower, or fasters, or the same? Same for all
+# What happens if you change the interval? divergences on [-1, 2] probabbly because the likelihood cant take negative parameters
+# What errors did you get
+np.random.seed(123)
+trials = 4
+theta_real = 0.35  # True (but unknown) probability of success
+data = pz.Binomial(n=1, p=theta_real).rvs(trials)  # Simulating observed data
+print("Simulated data:", data)
+uniform_params = [(0,1), (-1, 2)]
+idatasEx2 = []
+
+if __name__ == '__main__':
+    print('Fitting Beta')
+    with pm.Model() as our_first_model:
+        theta = pm.Beta('theta', alpha=1, beta=1)  # Prior for θ
+        y = pm.Bernoulli('y', p=theta, observed=data)  # Likelihood for observed data
+        idata = pm.sample(1000, random_seed=4591, cores=4, chains=4)  # Posterior sampling
+        idatasEx2.append(idata)
+
+    for lower, upper in uniform_params:
+        print(f'Fitting Uniform {[lower, upper]}')
+        with pm.Model() as our_first_model:
+            theta = pm.Uniform('theta', lower=lower, upper=upper)  # Prior for θ
+            y = pm.Bernoulli('y', p=theta, observed=data)  # Likelihood for observed data
+            idata = pm.sample(1000, random_seed=4591, cores=4, chains=4)  # Posterior sampling
+            idatasEx2.append(idata)
+
+    #Plot posterior for each model
+    _, axes = plt.subplots(1,3, figsize=(12, 4))
+    for idata, ax in zip(idatasEx2, axes):
+        az.plot_posterior(idata, ax=ax)
+    plt.show()
+    #Compare uncertainty around estimates
+    az.plot_forest(idatasEx2, combined=True, figsize=(10, 2))
+    plt.show()
+
+
+# %% Exercise 5
+# Modify model_g. Prior for the mean to a Gaussian centered at the empirical mean,
+# Try three sets of parameters for the sd.
+# How robust/sensitive are the infereneces to these changes? Very robust uncertainy increases with higher sd but they are all centered and have similar HDIs
+# What do you think of using a gaussian which is unboundeded when the data are bound to [0, 100]: Using an unbounded distirbution should be fine as long as the data themselves are bounded
+
+# Load Gaussian data (e.g., chemical shifts)
+data = np.loadtxt("D:\BAP\BAP3\code\data\chemical_shifts.csv") #Windows
+# data = np.loadtxt("/Users/uqamcka3/PHD/bap_Martin/BAP3/code/data/chemical_shifts.csv") #MAC
+empirical_mean = np.mean(data).round(2)
+sd_vals = [0.5, 1, 10]
+idatasEx3 = []
+#%%
+if __name__ == '__main__':
+    # Model specification and sampling for Gaussian inference
+    for sigma in sd_vals:
+        with pm.Model() as model_g:
+            mu = pm.Normal(f'mu_p{sigma}', mu=empirical_mean, sigma=sigma)  # Prior for μ
+            sigma = pm.HalfNormal('sigma', 5)  # Prior for σ
+
+            Y = pm.Normal('Y', mu=mu, sigma=sigma, observed=data)  # Likelihood
+            idata_g = pm.sample(random_seed=4591)  # Posterior sampling
+
+            idatasEx3.append(idata_g)
+
+    #Plot posterior for each model
+    _, axes = plt.subplots(1,3, figsize=(12, 4))
+    for idata, ax in zip(idatasEx3, axes):
+        az.plot_posterior(idata, ax=ax)
+    plt.show()
+    #Compare uncertainty around estimates
+    az.plot_forest(idatasEx3, combined=True, figsize=(10, 2))
+    plt.show()
+
+    #Summaries
+    summaries = [az.summary(idata, kind="stats") for idata in idatasEx3]
+    model_labels = [f'Prior_{i+1}' for i in range(len(summaries))]
+
+    df = create_summary_table(summaries, model_labels)
+
+# %% Exercise 6
+# Using the data from chem shifts, compute the mean and sd with and without outliers
+# Copmare results to the bayesian estimation using Gaussian and students t, what do you observe?
+# The bayesian results are similar to the empirical means of the data!!
+copy_data = data.copy()
+empirical_mean = np.mean(copy_data).round(2)
+empirical_sd = np.std(copy_data).round(2)
+
+outlier_mask = ~(np.abs(data-empirical_mean) < empirical_sd*2)
+copy_data[outlier_mask]
+empirical_mean_no_outliers = np.mean(copy_data[~outlier_mask])
+empirical_std_no_outliers = np.std(copy_data[~outlier_mask])
+
+idatasEx6 = []
+
+#Now run the models
+if __name__ == '__main__':
+    # Model specification and sampling for Gaussian inference
+    with pm.Model() as model_g:
+        mu = pm.Uniform('mu', lower=40, upper=70)  # Prior for μ
+        sigma = pm.HalfNormal('sigma', 5)  # Prior for σ
+
+        Y = pm.Normal('Y', mu=mu, sigma=sigma, observed=data)  # Likelihood
+        idata_g = pm.sample(random_seed=4591, cores=4, chains=4)  # Posterior sampling
+        idatasEx6.append(idata_g)
+
+    with pm.Model() as model_tr:
+        # Priors
+        mu = pm.Uniform('mu', lower=40, upper=75)  # Prior for μ
+        sigma = pm.HalfNormal('sigma', sigma=10)  # Prior for σ
+        nu = pm.Exponential('nu', 1 / 30)  # Prior for ν (degrees of freedom)
+
+        # Likelihood
+        y = pm.StudentT('y', nu=nu, mu=mu, sigma=sigma, observed=data)  # Robust likelihood
+
+        # Sampling from posterior
+        idata_tr = pm.sample(random_seed=4591, cores=4, chains=4)
+        idatasEx6.append(idata_tr)
+    
+    #Plot posterior for each model
+    _, axes = plt.subplots(1,2, figsize=(12, 4))
+    for idata, ax in zip(idatasEx6, axes):
+        az.plot_posterior(idata, ax=ax)
+    plt.show()
+    #Compare uncertainty around estimates
+    az.plot_forest(idatasEx6, combined=True, figsize=(10, 2))
+    plt.show()
+
+    #Summaries
+    summaries = [az.summary(idata, kind="stats") for idata in idatasEx6]
+    model_sums = create_summary_table(summaries)
+    print(f'Outlier {(empirical_mean, empirical_sd)}', '\n', f'NoOutlier: {(empirical_mean_no_outliers, empirical_std_no_outliers)}')
+# %% Exercise 7
+# Repeat the previous but add more outliers to chemical shifts and compute new posteriors
+#  what do you find? With a small amount of outliers proportional to your data the gaussian t is good at keeping the same results
+copy_data = data.copy()
+# Create an array with the outliers repeated 4 times
+additional_outliers = np.repeat(data[outlier_mask], 4)
+
+# Concatenate the original data array and the additional outliers
+data_more_outliers = np.concatenate([data, additional_outliers])
+empirical_mean = np.mean(data_more_outliers).round(2)
+empirical_sd = np.std(data_more_outliers).round(2)
+
+
+idatasEx7 = []
+
+#Now run the models
+if __name__ == '__main__':
+    # Model specification and sampling for Gaussian inference
+    with pm.Model() as model_g:
+        mu = pm.Uniform('mu', lower=40, upper=70)  # Prior for μ
+        sigma = pm.HalfNormal('sigma', 5)  # Prior for σ
+
+        Y = pm.Normal('Y', mu=mu, sigma=sigma, observed=data_more_outliers)  # Likelihood
+        idata_g = pm.sample(random_seed=4591, cores=4, chains=4)  # Posterior sampling
+        idatasEx7.append(idata_g)
+
+    with pm.Model() as model_tr:
+        # Priors
+        mu = pm.Uniform('mu', lower=40, upper=75)  # Prior for μ
+        sigma = pm.HalfNormal('sigma', sigma=10)  # Prior for σ
+        nu = pm.Exponential('nu', 1 / 30)  # Prior for ν (degrees of freedom)
+
+        # Likelihood
+        y = pm.StudentT('y', nu=nu, mu=mu, sigma=sigma, observed=data_more_outliers)  # Robust likelihood
+
+        # Sampling from posterior
+        idata_tr = pm.sample(random_seed=4591, cores=4, chains=4)
+        idatasEx7.append(idata_tr)
+    
+    #Plot posterior for each model
+    _, axes = plt.subplots(1,2, figsize=(12, 4))
+    for idata, ax in zip(idatasEx7, axes):
+        az.plot_posterior(idata, ax=ax)
+    plt.show()
+    #Compare uncertainty around estimates
+    az.plot_forest(idatasEx7, combined=True, figsize=(10, 2))
+    plt.show()
+
+    #Summaries
+    summaries = [az.summary(idata, kind="stats") for idata in idatasEx7]
+    model_sums = create_summary_table(summaries)
+    print(f'Outlier {(empirical_mean, empirical_sd)}')
+
+
+# %% Exercise 8
+# Explore the inferencedata object for idata_cg
+# How many groups does it have? 4
+# Inspect the posterior using sel method
+# Compute the distributions of mean differences between thrusday and sunday. 
+#   What are the coordinates and dimensions of the reuslting array
+tips = pd.read_csv('D:\\BAP\\BAP3\\code\data\\tips.csv')
+
+#Create variable representing tips in dollars
+categories = np.array(["Thur", "Fri", 'Sat', 'Sun'])
+tip = tips['tip'].values 
+# Create index variable that encodes days with numbers
+# [0, 1, 2, 3] instead of [’Thur’,’Fri’, ’Sat’, ’Sun’].
+idx = pd.Categorical(tips['day'], categories=categories).codes
+#In special cases we can specify coordinates to keep labels through the fitting proces
+coords = {'days': categories, 'days_flat': categories[idx]}
+# THis dictonary is actually a dictionary that holds one list called days which contains the labels
+
+if __name__ == '__main__':
+    with pm.Model(coords=coords) as comparing_groups: # NOTE THE COORDS MAPPPING HERE
+        # We cange from shape to dims, not sure why
+        #priors
+        mu = pm.HalfNormal('mu', sigma=5, dims='days') #Priors are VECTORS not Scalars
+        sigma = pm.HalfNormal('sigma', sigma=1, dims='days')
+
+        #Likelihood
+        #use our idx variable to pass our 4 categories into the 4 vector positions
+        y = pm.Gamma('y', mu=mu[idx], sigma=sigma[idx], observed=tip, dims='days_flat') #for some reason days flat here?
+
+        #sample the model
+        idata_cg = pm.sample(chains=4, cores=4, random_seed=4591)
+        #add posterior predictive
+        idata_cg.extend(pm.sample_posterior_predictive(idata_cg, random_seed=4591))
+
+        # Compute the distributions of mean differences between thrusday and sunday
+        posterior = az.extract(idata_cg.posterior)
+        TH_SdiffDist = posterior['mu'].sel(days='Thur') - posterior['mu'].sel(days='Sun')
+        az.plot_kde(TH_SdiffDist.values)
+        plt.show()
+        # Exercise 9
+        # Compute the probability of superiority directly from the posterior.
+        # Get our model's predictions for possible tips on Thursday and Sunday Flatten over all dimensions
+        posterior_predictive_thursday =  idata_cg.posterior_predictive["y"].sel(days_flat="Thur").stack(samples=("chain", "draw", "days_flat"))
+        posterior_predictive_sunday = idata_cg.posterior_predictive["y"].sel(days_flat="Sun").stack(samples=("chain", "draw", "days_flat"))
+
+        az.plot_kde(posterior_predictive_thursday.values, label="Thursday")
+        az.plot_kde(posterior_predictive_sunday.values, plot_kwargs={"color":"C1"}, label="Sunday")
+        plt.show() 
+        #the logic here is now we know how much overlap between the distributions is just though random draws from empirical distirbutions
+        thursday_tip_draws = np.random.choice(posterior_predictive_thursday, replace=True, size=1000)
+        sunday_tip_draws = np.random.choice(posterior_predictive_sunday, replace=True, size=1000)
+
+        (thursday_tip_draws > sunday_tip_draws).mean()
+        print((thursday_tip_draws > sunday_tip_draws).mean())
+
+        #Calculate cohens d for fun
+        cohens_d = (np.mean(posterior_predictive_thursday.values) - np.mean(posterior_predictive_sunday.values))/np.sqrt((np.std(posterior_predictive_sunday.values)+np.std(posterior_predictive_sunday.values))/2)
+        print(f"Cohens d: {cohens_d}")
+
+        #probability of superiority
+        ps = 0.5 * (1 + erf((cohens_d/np.sqrt(2) / np.sqrt(2))))
+        print(ps, (thursday_tip_draws > sunday_tip_draws).mean())
+# %%
--- code_AM/Chp_03.py
+++ code_AM/Chp_03.py
@@ -7,27 +7,33 @@ import pymc as pm
 import preliz as pz
 import pytensor
 
-pytensor.config.gcc__cxxflags = '-L/opt/miniconda3/envs/bap3/lib -O3 -march=native'
-pytensor.config.cxx = '/usr/bin/clang++'
-pytensor.config.blas__ldflags = '-framework Accelerate'
+# Only for Mac
+# pytensor.config.gcc__cxxflags = '-L/opt/miniconda3/envs/bap3/lib -O3 -march=native'
+# pytensor.config.cxx = '/usr/bin/clang++'
+# pytensor.config.blas__ldflags = '-framework Accelerate'
 
 #%% Important style code
 az.style.use("arviz-grayscale")
 from cycler import cycler
 default_cycler = cycler(color=["#000000", "#6a6a6a", "#bebebe", "#2a2eec"])
 plt.rc('axes', prop_cycle=default_cycler)
-plt.rc('figure', dpi=300)
+plt.rc('figure', dpi=100, figsize=(10, 6))
 np.random.seed(123)
 
 #%% Load theoretical NMR data
-# cs_data = pd.read_csv('D:\BAP\BAP3\code\data\chemical_shifts_theo_exp.csv')
-cs_data = pd.read_csv('/Users/uqamcka3/PHD/bap_Martin/BAP3/code/data/chemical_shifts_theo_exp.csv')
+cs_data = pd.read_csv('D:\BAP\BAP3\code\data\chemical_shifts_theo_exp.csv')
+# cs_data = pd.read_csv('/Users/uqamcka3/PHD/bap_Martin/BAP3/code/data/chemical_shifts_theo_exp.csv')
 diff = cs_data.theo - cs_data.exp
 cat_encode = pd.Categorical(cs_data['aa'])
 idx = cat_encode.codes
 coords = {"aa": cat_encode.categories}
 
-# Run a model where instead of any hierarchy we fit each group independently 
+## Now for our modelling we have multiple decisions
+# Hierarchical - Each amino acids are a fanmily of compounds so we assume they are all the same and estimate a single gaussian for all the differences
+# Non-Hierarchical - Each amino acid is different (there are 20 unique ones) so a better choice might be to fit 20 separate gaussians
+
+#%%
+# Run a model where instead of any hierarchy we fit each group independently (Unpooled) 
 if __name__ == '__main__':
     with pm.Model(coords=coords) as cs_nh:
         #priors
@@ -42,6 +48,162 @@ if __name__ == '__main__':
 
 
     param_Est = az.summary(idata_cs_nh, kind="stats").round(2)
+    print(param_Est)
     az.plot_trace(idata_cs_nh)
+    plt.show()
+
+# Run a model where we add a hierarchy (Hyper parameter)
+# This parameter is the distribution that represents the distribution of means of amino acids generally
+
+    with pm.Model(coords=coords) as cs_h:
+        #hyperpriors on mu
+        mu_hyp = pm.Normal('mu_hyp', mu=0, sigma=10)
+        sd_hyp = pm.HalfNormal('sd_hyp', 10)
+        # The above says that the hyperperior of mu's is normal with some mean and sd
+
+        #priors
+        mu = pm.Normal('mu', mu=mu_hyp, sigma=sd_hyp, dims='aa') #Not the use of the hierachical priors for mean and sd at individual level
+        sd = pm.HalfNormal('sd', sigma=10, dims='aa')
+
+        #likelihood
+        y = pm.Normal('y', mu=mu[idx], sigma=sd[idx], observed=diff)
+
+        #Sampling
+        idata_cs_h = pm.sample(random_seed=4591)
+
+
+    param_Esth = az.summary(idata_cs_h, kind="stats").round(2)
+    print(param_Esth)
+    az.plot_trace(idata_cs_h)
+    plt.show()
+
+    # Compare the models 
+    axes = az.plot_forest([idata_cs_nh, idata_cs_h], model_names=['non_hierarchical', 'hierarchical'],
+                        var_names='mu', combined=True, r_hat=False, ess=False, figsize=(10, 7),
+                        colors='cycle')
+    y_lims = axes[0].get_ylim()
+    axes[0].vlines(idata_cs_h.posterior['mu_hyp'].mean(), *y_lims, color="k", ls=":")
+    plt.show()
+# %% Water Quality Example
+# Analyse quality of water in a city, 
+# samples taken by dividing city into neighborhoods
+# We have two things we can do:
+# - Study each neighbourhood as a seperate entity
+# - Pool all the data together and estimate teh water quality of the city as a single big group
+
+# By using a pooled (Hierarchical) Model we can do both (estimate the neigbourhoods and the group level)
+
+# Simulate the data
+# 3 Neighbourhoods
+
+N_samples = [30, 30, 30]  # Number of samples collected in each group/neighborhood
+# Good (1) is below WHO lead recommendations, Bad (0) is above 
+G_samples = [18, 18, 18]  # Number of good quality water samples in each neighborhood
+
+group_idx = np.repeat(np.arange(len(N_samples)), N_samples)  
+# Generate a list of group indices where each sample belongs (e.g., group 0, 1, or 2)
+# `np.arange(len(N_samples))` creates indices [0, 1, 2] for the three neighborhoods.
+# `np.repeat` replicates each index based on the number of samples in that group.
+
+data = []  # Initialize an empty list to store the data (binary values: 1 for good water, 0 for bad water)
+for i in range(0, len(N_samples)):  # Loop over the number of groups (neighborhoods)
+    data.extend(np.repeat([1, 0], [G_samples[i], N_samples[i] - G_samples[i]]))
+    # For each group, create a list of 1s (good water samples) and 0s (bad water samples)
+    # `G_samples[i]` specifies the number of good samples
+    # `N_samples[i] - G_samples[i]` specifies the number of bad samples
+    # `np.repeat([1, 0], [good_count, bad_count])` creates a list with the specified number of 1s and 0s
+    # `data.extend` appends this list to the main `data` list
+
+# %% Modelling
+# We will be using this model
+# Likelihood : y_i ~ Bernoulli(theta_i)
+# Prior : theta_i ~ beta(mu, nu)
+# Hyperprior on nu : nu ~ HalfNormal(sigma_nu)
+# Hyperprior on mu : mu ~ Beta(alpha_mu, beta_mu)
+
+if __name__ == '__main__':
+    with pm.Model() as model_h:
+        #hyper priors
+        mu = pm.Beta('mu', 1, 1) #City level (group) mean
+        nu = pm.HalfNormal('nu', 10) #City level (group) concentration
+
+        #priors
+        theta = pm.Beta('theta', mu=mu, nu=nu, shape=len(N_samples)) # A theta per neighbour hood
+
+        #likelihood
+        y = pm.Bernoulli('y', p=theta[group_idx], observed=data)
+
+        idata_h = pm.sample()
+
+    param_Est_water = az.summary(idata_h, kind="stats").round(2)
+    print(param_Est_water)
+    az.plot_trace(idata_h)
+    plt.show()
+
+    posterior = az.extract(idata_h, num_samples=100)
+    for sample in posterior[["mu", "nu"]].to_array().values.T:
+        pz.Beta(mu=sample[0], kappa=sample[1]).plot_pdf(legend=None, color="C0", alpha=0.1, support=(0.01, 0.99), moments="m")
+
+    pz.Beta(mu=posterior["mu"].mean().item(), kappa=posterior["nu"].mean().item()).plot_pdf(legend=None, color="C0", moments="m")
+    plt.xlabel('$theta_{prior}$')
+# %% Soccer Player Positions
+# Premier League data set
+# Interested in goals per shot (success rate)
+
+# Model :
+#Likelihood : gs ~ Binomial(theta_i)
+
+#Prior on theta_i : theta _i ~ Beta(theta_pos)
+# Beta(theta_pos) = Beta(mu_p, nu_p)
+
+# Hyperprior on mu_p : mu_p ~ beta(mu, v)
+# Hyperprior on mu ~ beta(1.7, 5.8)
+# Hyperprior on nu ~ gamma(mu=125, sigma=50)
+football = pd.read_csv("D:\BAP\BAP3\code\data\\football_players.csv", dtype={'position':'category'})
+print(football.head)
+
+pos_idx = football.position.cat.codes.values
+pos_codes = football.position.cat.categories
+n_pos = pos_codes.size
+n_players = football.index.size
+print(pos_codes)
+coords = {'pos': pos_codes}
+print(coords)
+# %% Modelling
+if __name__ == "__main__":
+    with pm.Model(coords=coords) as model_football:
+        # Hyperpriors
+        nu = pm.Gamma('nu', mu=125, sigma=50)
+        mu = pm.Beta('mu', 1.7, 5.8)
+
+        # Postition Parameters on theta_p for theta_i (success rate %)
+        # vector of 4 mu's for each pos DF, FW, GK, MF
+        mu_p = pm.Beta('mu_p', mu=mu, nu=nu, dims='pos')
+        nu_p = pm.Gamma('nu_p', mu=125, sigma=50, dims='pos')
+
+        #Player parameters
+        #pos idx is how we match the vector of 4 to the same value in our data chat gpt explain this more and give me an example how how this linking is working with 2 minimal reproducable examples
+        theta_i = pm.Beta('theta_i', mu=mu_p[pos_idx], nu=nu_p[pos_idx])
+
+        # Likelihood which we seem not to care about? Why is this?
+        _ = pm.Binomial('gs', n=football.shots.values, p=theta_i, observed=football.goals.values)
+        
+        idata_football = pm.sample()
+
+
+    print(idata_football.posterior)
+    param_Est_football = az.summary(idata_football, kind="stats").round(2)
+    print(param_Est_football)
+
+    _, ax = plt.subplots(3, 1, figsize=(12, 6), sharex=True)
+    az.plot_posterior(idata_football, var_names='mu', ax=ax[0])
+    ax[0].set_title(r"Global mean")
+    az.plot_posterior(idata_football.posterior.sel(pos="FW"), var_names='mu_p', ax=ax[1])
+    ax[1].set_title(r"Forward position mean")
+    az.plot_posterior(idata_football.posterior.sel(theta_i_dim_0=1457), var_names='theta_i', ax=ax[2])
+    ax[2].set_title(r"Messi mean")
+    plt.show()
+
+    az.plot_forest(idata_football, var_names=['mu_p'], combined=True, figsize=(12, 3))
     plt.show()
 # %%
